193151fe13152a76224522603a6e95a516052ff1
JJonas1998 <Jonas.Jahrstorfer@student.hswt.de>
2025-05-22 12:08:12 +0200

    Basiscodes

---
+++ Projekt 9: Simulation einer Temperaturregelung von Bioreaktoren.py
@@ -0,0 +1,13 @@
+
+#### Projekt 9: Simulation einer Temperautrregelung von Bioreaktoren
+
+### Ersteller/-in: Jonas Jahrstorfer, Johanna Niklas
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+
+## Bioreaktor-Klasse
+# Schreiben Sie eine Python-Funktion zur Simulation von Temperaturänderungen in einem Bioreaktor
+
